71de9e61b8d858e3cf3f2a8f8cd3014d3a3cc608
swazkit <swastikraychoudhuri@gmail.com>
2022-12-06 22:43:54 +0530

    enemies initialised

--- app.js
+++ app.js
@@ -42,6 +42,28 @@ class Projectile {
         this.y = this.y + this.velocity.y
     }
 }
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false)
+        c.fillStyle = this.color
+        c.fill();
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 const x = canvas.width / 2
 const y = canvas.height / 2
@@ -50,6 +72,30 @@ const player = new Player(x, y, 30, 'blue')
 
 
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = 30
+        let x
+        let y
+        if(Math.random() < 0.5){
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+        y = Math.random() * canvas.height
+        } else {
+        x = Math.random() * canvas.width
+        y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+        const color = 'green'
+        const angle  = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+        console.log(enemies);
+    }, 1000);
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -59,6 +105,10 @@ function animate() {
         {
             projectile.update()
         })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })
 }
 
 addEventListener('click', (event) => {
@@ -71,4 +121,7 @@ addEventListener('click', (event) => {
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity))
 })
 
+
+
 animate()
+spawnEnemies()
