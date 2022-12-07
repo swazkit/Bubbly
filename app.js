e12f5772ef30012cab9b8ceb2dd7c0dd85db61d3
swazkit <swastikraychoudhuri@gmail.com>
2022-12-07 18:39:27 +0530

    minor style changes

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, '#FFFFFF')
 
 
 const projectiles = []
@@ -86,7 +86,7 @@ function spawnEnemies() {
         x = Math.random() * canvas.width
         y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle  = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
         const velocity = {
             x: Math.cos(angle),
@@ -100,7 +100,8 @@ function spawnEnemies() {
 let animationId
 function animate() {
     animationId = requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle = "rgba(0, 0, 0, 0.1)"
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.forEach((projectile, index) => 
         {
@@ -141,10 +142,10 @@ function animate() {
 addEventListener('click', (event) => {
     const angle  = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 4,
+        y: Math.sin(angle) * 4
     }
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity))
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "#FFFFFF", velocity))
 })
 
 
